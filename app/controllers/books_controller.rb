591041fd34ded4a683e67e143496029b54b1bfb7
Dan Roberts <dan@adadevelopersacademy.org>
2019-04-08 16:03:39 -0700

    Test and implement the books#show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,36 @@
-BOOKS = [
-  {title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  {title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  {title: "Kindred", author: "Octavia E. Butler"},
-]
+
 
 class BooksController < ApplicationController
+  BOOKS = [
+    {title: "Hidden Figures", author: "Margot Lee Shetterly"},
+    {title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+    {title: "Kindred", author: "Octavia E. Butler"},
+  ]
+
   def index
     # Load a list of books from somewhere
     @books = BOOKS # Book.all
+
+    @featured_book = @books.sample
   end
+
+  def show
+    book_id = params[:id]
+    puts "Book ID was #{book_id}"
+    puts "Result of .to_i: #{book_id.to_i}"
+    # binding.pry
+
+    does_not_exist
+
+    @book = BOOKS[book_id.to_i]
+    # @book = Book.find(book_id)
+
+    unless @book
+      head :not_found
+    end
+  end
+
+  # def test
+  #   binding.pry
+  # end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 describe BooksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "renders without crashing" do
+      # Arrange
+
+      # Act
+      get "/books"
+
+      # Assert
+      must_respond_with :ok
+    end
+
+    it "renders even if there are zero books" do
+      skip
+      # Arrange
+      # TODO get rid of the books
+      BooksController::BOOKS.clear
+      # Book.destroy_all
+
+      # Act
+      get "/books"
+
+      # Assert
+      must_respond_with :ok
+    end
+  end
+
+  describe "show" do
+    it "returns a 404 status code if the book doesn't exist" do
+      # TODO come back to this
+      book_id = 12345
+
+      get "/books/#{book_id}"
+
+      must_respond_with :not_found
+    end
+
+    it "works for a book that exists" do
+      # TODO come back to this
+      book_id = 0
+
+      get "/books/#{book_id}"
+
+      must_respond_with :ok
+    end
+  end
 end
