9b29c70f5fcad31f97b483871423dbd5653d5dd1
Devin Helmgren <devincollege@gmail.com>
2019-04-16 13:56:54 -0700

    merged my stash

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,18 @@
 class Author < ApplicationRecord
   has_many :books
 
+
   def cool_new_method ()
     puts "wow, neat"
 
     puts "this is a bug"
+
+  end
+
+  def first_published
+    books_with_year = self.books.where.not(publication_date: nil)
+    first_book = books_with_year.order(publication_date: :asc).first
+    return first_book.publication_date
 
   end
 end
