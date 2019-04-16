037cfaffc6a3e68477463e50c5be37376362c152
Devin Helmgren <devincollege@gmail.com>
2019-04-16 14:09:18 -0700

    Git ridda this trash

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,14 +1,6 @@
 class Author < ApplicationRecord
   has_many :books
 
-
-  def cool_new_method ()
-    puts "wow, neat"
-
-    puts "this is a bug"
-
-  end
-
   def first_published
     books_with_year = self.books.where.not(publication_date: nil)
     first_book = books_with_year.order(publication_date: :asc).first
