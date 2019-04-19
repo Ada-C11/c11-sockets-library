f2063788df1a806309e72127c9d8ac2f996c8c2d
Dan Roberts <dan@adadevelopersacademy.org>
2019-04-19 11:27:50 -0700

    Add genre model, many-to-many relation with books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_184454) do
+ActiveRecord::Schema.define(version: 2019_04_19_181649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 2019_04_15_184454) do
     t.string "description"
     t.bigint "author_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "books_genres", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "genre_id"
+    t.index ["book_id"], name: "index_books_genres_on_book_id"
+    t.index ["genre_id"], name: "index_books_genres_on_genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "books", "authors"
