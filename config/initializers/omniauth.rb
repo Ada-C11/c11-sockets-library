7258916bff7bbce2c1f704f08a5f53f944e267d0
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-08 19:41:45 -0700

    Don't attempt to do dotenv stuff in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
-Dotenv.load
+if ["development", "test"].include? ENV["RAILS_ENV"]
+  Dotenv::Railtie.load
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
